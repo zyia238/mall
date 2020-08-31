79816103ee9ba62da5807ab2d1c94ce61e6f8512
zyia238 <278767942@qq.com>
2020-08-31 16:31:10 +0800

    修复报错问题 添加分类的tabcontrol栏

--- src/views/category/subcategory.vue
+++ src/views/category/subcategory.vue
@@ -1,43 +1,47 @@
 <template>
   <div>
-      <sub-category-list :subs="subs"/>
+    <sub-category-list :subs="subs" />
+    <category-list-more></category-list-more>
   </div>
 </template>
 
 <script>
-import {getSubCategoryData} from '../../network/category'
-import subCategoryList from './childComponents/subCategoryList'
+import { getSubCategoryData } from "../../network/category";
+import subCategoryList from "./childComponents/subCategoryList";
+import categoryListMore from "./childComponents/categoryListMore";
 
 export default {
-  name: 'subcategory',
-  data(){
-      return {
-          subs:[]
-      }
+  name: "subcategory",
+  data() {
+    return {
+      subs: []
+    };
   },
-  components:{
-      subCategoryList
+  components: {
+    subCategoryList,
+    categoryListMore
   },
-  mounted(){
-      this.subs = [];
-        getSubCategoryData(this.$route.params.maitKey).then(res => {
+  mounted() {
+    this.subs = [];
+    if (this.$route.params.maitKey) {
+      getSubCategoryData(this.$route.params.maitKey).then(res => {
         this.subs.push(...res.data.data.list);
-        console.log(this.subs)
-      });
-  },
-  watch: {
-    $route(to,from){
-        
-        this.subs = [];
-        getSubCategoryData(this.$route.params.maitKey).then(res => {
-        this.subs.push(...res.data.data.list);
-        
+        // console.log(this.subs);
       });
     }
+  },
+  watch: {
+    $route(to, from) {
+      this.subs = [];
+      if (this.$route.params.maitKey) {
+        getSubCategoryData(this.$route.params.maitKey).then(res => {
+          this.subs.push(...res.data.data.list);
+        });
+      }
+    }
   }
-}
+};
 </script>
 
 <style lang="" scoped>
-
 </style>
