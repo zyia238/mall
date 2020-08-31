e093de2666185c7278dc5fc8cd4fd607aa2c6533
zyia238 <278767942@qq.com>
2020-08-31 16:25:10 +0800

    增加分类的tabcontrol栏

---
+++ src/views/category/childComponents/categoryListMore.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <tab-control :titles="['综合','新品','销量']" />
+  </div>
+</template>
+
+<script>
+import tabControl from '../../../components/common/tabcontrol'
+
+export default {
+  name: '',
+  components:{
+      tabControl
+  },
+  data () {
+    return {}
+  },
+  created () {},
+  // mounted () {},
+  computed: {},
+  watch: {},
+  methods: {}
+}
+</script>
+
+<style lang="" scoped>
+
+</style>
